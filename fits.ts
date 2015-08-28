d396477021a22185ef60824a7dc510cab7b99f47
Chris Lucas <chris@chrisjlucas.com>
2015-08-27 17:44:20 -0700

    Added support for null and undefined

--- fits.ts
+++ fits.ts
@@ -7,6 +7,7 @@ var sprintf = require('sprintf-js').sprintf;
 class Keyword {
     private static CARD_LENGTH = 80;
     private static MAX_KEYWORD_LEN = 8;
+    private static MAX_VALUE_LEN = 70;
 
     constructor(private name: string,
             private value: any,
@@ -21,17 +22,28 @@ class Keyword {
         s += '= ';
 
         var value: string;
-
-        if (typeof this.value == 'bool') {
+        console.log(typeof this.value);
+        if (typeof this.value == 'boolean') {
             value = this.value ? 'T' : 'F';
         } else if (typeof this.value == 'number') {
             var fmt = this.value === +value ? '%d' : '%e';
             // sprintf-js doesnt support %E, so hack away
             value = sprintf(fmt, this.value).replace('e', 'E');
         } else if (this.value === null) {
+            value = "''";
+        } else if (this.value === undefined) {
             value = '';
         } else {
-            value = sprintf("'%s'", this.value);
+            value = this.value.toString();
+            if (value.length == 0) {
+                value = ' ';
+            }
+
+            value = `'${value}'`;
+        }
+
+        if (value.length > Keyword.MAX_VALUE_LEN) {
+            throw new Error(`Value ${value} exceeds ${Keyword.MAX_VALUE_LEN} character limit`); 
         }
 
         s += sprintf('%20s ', value);
--- test/test.ts
+++ test/test.ts
@@ -5,30 +5,73 @@ import chai = require('chai');
 
 var assert = chai.assert;
 
-// ES6 polyfill
-if (!String.prototype.includes) {
-    String.prototype.includes = function() {'use strict';
-        return String.prototype.indexOf.apply(this, arguments) !== -1;
-    };
-}
-
 function assertCardMatches(card: string, expectedName: string, expectedValue: string, expectedComment?: string): void {
-    assert.equal(card.length, 80, 'Card length should be 80 characters');
-    assert.equal(card.substr(0, 8), 'TARGET  ', 'Keyword did not match');
-    assert.equal(card.substr(8, 2), '= ', 'Value indicator not found');
+    var errMsg: string = null;
 
-    var valComm = card.substr(10);
-    assert.isTrue(valComm.includes(expectedValue), `Expected value $expectedValue not found in card`);
+    if (card.length < 80) {
+        errMsg = 'Card length should be 80 characters';
+    }
+    if (card.substr(0, 8) !== expectedName) {
+        errMsg = 'Keyword did not match';
+    }
 
-    if (expectedComment) {
-        assert.isTrue(valComm.includes(expectedComment), `Expected value $expectedComment not found in card`);
+    if (card.substr(8, 2) !== '= ') {
+        errMsg = 'Value indicator not found';
+    }
+
+    var valComm = card.substr(10).trim();
+
+    // If expected value but no value found (left-trimmed to comment)
+    if (typeof expectedValue === 'string' && (valComm[0] == '/'
+        || valComm.substr(0, expectedValue.length) !== expectedValue) {
+        errMsg = `Expected value ${expectedValue} not found in card`;
+    }
+
+    var comm = valComm.substr(expectedValue.length).trim();
+
+    if (expectedComment && (comm.length == 0 || comm !== expectedComment)) {
+        errMsg = `Expected value "${expectedComment}" not found in card`;
+    }
+
+    if (errMsg != null) {
+        assert.fail(card, null, errMsg + `\nActual: "${card}"`); 
     }
 }
 
-describe('thing', () => {
-    it('should be true', (done) => {
+describe('#asCard', () => {
+    it('should return a valid card with string value', (done) => {
         assertCardMatches(new fits.Keyword('TARGET', 'TSTTGT', null).asCard(),
-                'TARGET', '\'TSTTGT\'', null);
+                'TARGET  ', '\'TSTTGT\'', null);
+        done();
+    });
+    
+    it('should return a valid card with true boolean', (done) => {
+        assertCardMatches(new fits.Keyword('TARGET', true, null).asCard(),
+                'TARGET  ', 'T', null);
+        done();
+    });
+    
+    it('should return a valid card with false boolean', (done) => {
+        assertCardMatches(new fits.Keyword('TARGET', false, null).asCard(),
+                'TARGET  ', 'F', null);
+        done();
+    });
+    
+    it('should return a valid card with a null string', (done) => {
+        assertCardMatches(new fits.Keyword('TARGET', null, null).asCard(),
+                'TARGET  ', "''", null);
+        done();
+    });
+    
+    it('should return a valid card with a empty string', (done) => {
+        assertCardMatches(new fits.Keyword('TARGET', '', null).asCard(),
+                'TARGET  ', "' '", null);
+        done();
+    });
+    
+    it('should return a valid card with an undefined keyword', (done) => {
+        assertCardMatches(new fits.Keyword('TARGET', undefined, null).asCard(),
+                'TARGET  ', undefined, null);
         done();
     });
 });
